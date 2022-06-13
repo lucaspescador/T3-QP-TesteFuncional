a9452a63154d34944c7a0c44f8d57d957d772c24
Lucas Camargo <lucaspescadordecamargo@gmail.com>
2022-06-13 01:43:34 -0300

    Ajuste versão chromedriver

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -12,14 +12,11 @@ namespace TestProject1
         [Fact]
         public void Test1()
         {
-            //arrange - dado que o navegador está aberto
             IWebDriver driver = new ChromeDriver(
             Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
 
-            //act - quando navegamos para a URL https://www.caelum.com.br
             driver.Navigate().GoToUrl("https://www.caelum.com.br");
 
-            //assert - então esperamos que a página apresentada seja da Caelum
             Assert.Contains("Caelum", driver.Title);
         }
     }
