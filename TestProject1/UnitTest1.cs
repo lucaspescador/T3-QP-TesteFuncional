3a9dfb38216a23bb0b68410d23de90154acc60d3
Lucas Camargo <lucaspescadordecamargo@gmail.com>
2022-06-13 13:09:54 -0300

    Teste Lucas
    Tentativa de login e processo de solicitação de recuperação de e-mail

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -15,9 +15,33 @@ namespace TestProject1
             IWebDriver driver = new ChromeDriver(
             Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
 
-            driver.Navigate().GoToUrl("https://www.caelum.com.br");
+            //Acessar o site do moodle
+            driver.Navigate().GoToUrl("https://moodle.pucrs.br/");
 
-            Assert.Contains("Caelum", driver.Title);
+            //Aguardar até aparecer a palavra "Moodle" no título do site.
+            Assert.Contains("Moodle", driver.Title);
+
+            //Preencher campo login com o email testelucas@gmail.com
+            IWebElement login = driver.FindElement(By.Id("login_username"));
+            login.SendKeys("testelucas@gmail.com");
+
+            //Preencher senha com o valor 123456 e pressionar o botão enter.
+            IWebElement password = driver.FindElement(By.Id("login_password"));
+            password.SendKeys("123456");
+            password.SendKeys(Keys.Enter);
+
+            //Aguardar até aparecer a palavra "Pontifícia" no título do site
+            Assert.Contains("Pontifícia", driver.Title);
+
+            //Clicar no elemento que possui o texto "Esqueceu o seu usuário ou senha?"
+            driver.FindElement(By.LinkText("Esqueceu o seu usuário ou senha?")).Click();
+
+            //Preencher com o email 
+            driver.FindElement(By.Id("id_email")).SendKeys("testelucas@gmail.com");
+
+            //Clicar no botão buscar
+            driver.FindElement(By.Name("submitbuttonemail")).Click();
+            
         }
     }
 }
